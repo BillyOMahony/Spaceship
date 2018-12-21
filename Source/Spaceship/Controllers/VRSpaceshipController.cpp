c15ae4d12965188411711dac07ccb3ab8e994074
Billy O'Mahony <omahonybilly@gmail.com>
2018-12-21 12:55:16 +0000

    Can now Aim in VR

--- Source/Spaceship/Controllers/VRSpaceshipController.cpp
+++ Source/Spaceship/Controllers/VRSpaceshipController.cpp
@@ -1,8 +1,57 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "VRSpaceshipController.h"
+#include "Components/StaticMeshComponent.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/Engine.h"
+#include "Weapon/SpaceshipWeaponsComponent.h"
+
+void AVRSpaceshipController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimTowardsAimLocation();
+}
 
 void AVRSpaceshipController::SetHandMeshes(UStaticMeshComponent * LeftHandMesh, UStaticMeshComponent * RightHandMesh)
 {
-	
+	UE_LOG(LogTemp, Error, TEXT("Meshes Assigned"));
+	this->LeftHandMesh = LeftHandMesh;
+	this->RightHandMesh = RightHandMesh;
+}
+
+bool AVRSpaceshipController::GetAimLocation(FVector & HitLocation)
+{
+	if (!LeftHandMesh) return false;
+
+	FHitResult HitResult;
+	FVector StartLocation = LeftHandMesh->GetComponentLocation();
+	FVector AimDirection = LeftHandMesh->GetComponentRotation().Vector().GetSafeNormal();
+
+	if(GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, StartLocation + (AimDirection * RayTraceRange), ECollisionChannel::ECC_Visibility))
+	{
+		HitLocation = HitResult.Location;
+		if (HitResult.GetActor() != GetPawn()) return true;
+	}
+
+	HitLocation = StartLocation + (AimDirection * RayTraceRange);
+	return true;
+}
+
+void AVRSpaceshipController::AimTowardsAimLocation()
+{
+	if (!GetPawn()) { return; } // if not possessing 
+	USpaceshipWeaponsComponent * WeaponsComponent = GetPawn()->FindComponentByClass<USpaceshipWeaponsComponent>();
+	if (!ensure(WeaponsComponent)) { return; }
+
+	FVector HitLocation; // Out parameter
+	if (GetAimLocation(HitLocation)) {
+		DrawLine(LeftHandMesh->GetComponentLocation(), HitLocation);
+		WeaponsComponent->AimAt(HitLocation);
+	}
+}
+
+void AVRSpaceshipController::DrawLine(FVector StartPoint, FVector EndPoint)
+{
+	DrawDebugLine(GetWorld(), StartPoint, EndPoint, FColor::Blue, false, -1, 0, 2.f);
 }
--- Source/Spaceship/Controllers/VRSpaceshipController.h
+++ Source/Spaceship/Controllers/VRSpaceshipController.h
@@ -15,10 +15,32 @@ class SPACESHIP_API AVRSpaceshipController : public APlayerController
 	GENERATED_BODY()
 	
 public:
+	virtual void Tick(float DeltaTime) override;
+
+	/*
+	 *
+	 */
 	UFUNCTION(BlueprintCallable)
 	void SetHandMeshes(UStaticMeshComponent * LeftHandMesh, UStaticMeshComponent * RightHandMesh);
 
 private:
+	UPROPERTY(EditAnywhere)
+	float RayTraceRange = 50000.f;
+
 	UStaticMeshComponent * LeftHandMesh, * RightHandMesh;
 
+	/*
+	 *
+	 */
+	bool GetAimLocation(FVector &HitLocation);
+
+	/*
+	 *
+	 */
+	void AimTowardsAimLocation();
+
+	/*
+	 *
+	 */
+	void DrawLine(FVector StartPoint, FVector EndPoint);
 };
