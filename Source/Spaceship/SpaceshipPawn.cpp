9d7a9ee6b9f1cc054a328da8e48b70f4641bc5c0
Billy O'Mahony <omahonybilly@gmail.com>
2019-01-14 16:18:20 +0000

    SpaceshipPawn takes damage

--- Source/Spaceship/SpaceshipPawn.cpp
+++ Source/Spaceship/SpaceshipPawn.cpp
@@ -15,6 +15,26 @@ void ASpaceshipPawn::BeginPlay()
 	Super::BeginPlay();
 }
 
+void ASpaceshipPawn::OnDeath()
+{
+	//TODO Implement
+	UE_LOG(LogTemp, Error, TEXT("ASpaceshipPawn::OnDeath() - DEADDDDDD"));
+		
+}
+
+float ASpaceshipPawn::TakeDamage(float DamageAmount, FDamageEvent const & DamageEvent, AController * EventInstigator, AActor * DamageCauser)
+{
+	Health -= DamageAmount;
+
+	if(Health <= 0)
+	{
+		Health = 0;
+		OnDeath();
+	}
+
+	return DamageAmount;
+}
+
 void ASpaceshipPawn::ToggleFirstPerson()
 {
 	bFirstPerson = !bFirstPerson;
@@ -25,6 +45,11 @@ void ASpaceshipPawn::SetIsVirtualReality(bool VirtualReality)
 {
 	bVirtualReality = VirtualReality;
 	if (bVirtualReality) ToggleFirstPerson();
+}
+
+float ASpaceshipPawn::GetHealth()
+{
+	return Health;
 }
 
 // Called every frame
--- Source/Spaceship/SpaceshipPawn.h
+++ Source/Spaceship/SpaceshipPawn.h
@@ -15,15 +15,44 @@ public:
 	// Sets default values for this pawn's properties
 	ASpaceshipPawn();
 
+	/*
+	 *	Applies damage to this spaceship
+	 */
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
+
+	/*
+	 *	Toggles the camera
+	 *	Switches between third and first person
+	 */
 	UFUNCTION(BlueprintCallable)
 	void ToggleFirstPerson();
 
+	/*
+	 *	Sets whether or not VR is in use
+	 */
 	UFUNCTION(BlueprintCallable)
 	void SetIsVirtualReality(bool VirtualReality);
+
+	/*
+	 *	@return Spaceship health value
+	 */
+	UFUNCTION(BlueprintCallable)
+	float GetHealth();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	/*
+	 *	Handles death of a pawn
+	 */
+	void OnDeath();
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	bool bFirstPerson = true;	
@@ -31,11 +60,5 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	bool bVirtualReality = true;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	float Health = 100.f;
 };
